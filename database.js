b99418080ed4c1d30f7e9028ba984f75f40d5a06
Jason Sarrio <jsarrio16@gmail.com>
2020-08-10 14:30:49 -0500

    MVP HIT

--- database.js
+++ database.js
@@ -1,45 +1,64 @@
+const shortid = require("shortid");
+
 let users = [
-	{ id: "1", name: "Jane Doe" },
-	{ id: "2", name: "John Doe" },
-	{ id: "3", name: "Jack Doe" },
-]
+    {
+        id: "1",
+        name: "Jane Doe",
+        bio: "Jane bio",
+    },
+    {
+        id: "2",
+        name: "John Doe",
+        bio: "John bio",
+    },
+    {
+        id: "3",
+        name: "Jack Doe",
+        bio: "Jack bio",
+    },
+    {
+        id: "4",
+        name: "Mark Ambro",
+        bio: "Mark bio",
+    },
+];
 
 function getUsers() {
-	return users
+    return users;
 }
 
 function getUserById(id) {
-	return users.find(u => u.id === id)
+    return users.find((u) => u.id === id);
 }
 
 function createUser(data) {
-	const payload = {
-		id: String(users.length + 1),
-		...data,
-	}
+    const payload = {
+        id: String(users.length + 1),
+        ...data,
+    };
 
-	users.push(payload)
-	return payload
+    users.push(payload);
+    return payload;
 }
 
 function updateUser(id, data) {
-	const index = users.findIndex(u => u.id === id)
-	users[index] = {
-		...users[index],
-		...data,
-	}
-	
-	return users[index]
+    const index = users.findIndex((u) => u.id === id);
+    users[index] = {
+        ...users[index],
+        ...data,
+    };
+
+    return users[index];
 }
 
 function deleteUser(id) {
-	users = users.filter(u => u.id != id)
+    users = users.filter((u) => u.id != id);
 }
 
 module.exports = {
-	getUsers,
-	getUserById,
-	createUser,
-	updateUser,
-	deleteUser,
-}
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+};
